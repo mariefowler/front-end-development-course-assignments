8d0704c9e746e4ad003bb17949257bb12f4bb25a
mariefowler <mfowler@sdale.org>
2016-11-19 09:44:22 -0600

    Commit
    Started over main2.js

--- Assignment12/scripts/main.js
+++ Assignment12/scripts/main.js
@@ -1,25 +1,70 @@
 
 var btn = getE('btnSubmit');
+btn.addEventListener('click', update);
 
-//var btnR = getE('Reset');
-//btnR.addEventListener("click", reset);
+   function update(){
+    //creates empty array
+    var errorMsg = [];
+   
+    var firstName = getElm('firstName');
+    if(firstName.value===''){
+        errorMsg.push('First Name is required');
+        firstName.classList.add(errClass);
+    }
+   { 
+    var lastName = getElm('lastName');
+    if(lastName.value===''){
+        errorMsg.push('Last Name is required');
+        lastName.classList.add(errClass);
+    }
+   { 
+    var email = getElm('emailAddress');
+    if(emailAddress.value===' '){
+        errorMsg.push('email is required');
+        email.classList.add(errClass);
 
-///function submitfunc(){
- //  alert(this.id+ " : you clicked Submit")
-//};
+    }else if (email.value.indexOf('@') == -1 ||
+        email.value.indexOf('.com') == -1 {
+        errorMsg.push('A Valid Email is required');
+        email.classList.add(errClass);
+    }
 
-//function reset(){
-//    alert('you clicked Reset')
+    }
+   var password = getElm('password');
+   if(password.value== ' '){
+       errorMsg.push('Password is required');
 
-//};
+   }
+   var passwordConfirm = getElm('passwordConfirm');
+   if (passwordConfirm.value === ' ') {
+       errorMsg.push('Confirm is required');
+       passwordConfirm. classList.add(errClass);
+   }
+   
+   If (password.value !== ' ' &&
+       passwordConfirm.value !== ' ' &&
+       password.value !== passwordConfirm.value){
 
-btn.addEventListener('click,' submit );
+           errorMsg.push('Passwords must match');
 
-function myFunction() {
-    alert("You clicked Submit");
+           password.classList.add(errClass);
+           passwordConfirm.clssList.add(errClass);
 
+       }
+   
+   
+   function getElm(id) {
+        return document.getElementbyId(id);
+    }
 
+   var resetBtn = getElm('btnReset');
+   resetBtn.addEventListener('click', reset);
 
+   function reset(){
+       var input IdArray = ['firstName', 'lastName', 'emailAddress', 'password', 'passwordconfirm' ]
+
+       for(var i=0; i< inputIdArray)
+   }
 
    // document.getElementbyID('Submit').addEventListener("click", btnSubFunc () 
 //});
---
+++ Assignment12/scripts/main2.js
@@ -0,0 +1,32 @@
+// JavaScript source code
+
+var btn;
+    document.getElementById('btnSubmit').addEventListener('onClick', update);
+
+function update() {
+    alert('You have Submitted the Form');
+
+}
+
+var btn2;
+    document.getElementById('btnReset').addEventListener('onClick', reset);
+
+    function reset() {
+        alert('Form has been Reset');
+
+    }
+   
+     
+    
+
+
+            
+
+   
+     /*   
+    var lastName = document.getElementById('lastNam');
+    var email = document.getElementById('email');
+    var password = document.getElementById('passWd');
+    var confirmPass = document.getElementById('confirmPass');
+    */
+
