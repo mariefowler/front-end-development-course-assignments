9dd126c5aad19d1b0d4671369756148920d4a5c3
mariefowler <mfowler@sdale.org>
2016-12-08 17:43:47 -0600

    Comitt

--- Assignment17/scripts/main.js
+++ Assignment17/scripts/main.js
@@ -3,3 +3,44 @@ $(document).ready(function(){
 
 
 });
+
+$('span').hide();//$('.error').hide();
+
+
+$('.input-group').filter(':even').addClass('even');
+
+$('#firstName').on('blur', function(){
+    var input = $(this);
+
+    if (input.val() === '') {
+        input.closest('.input-group').find('.requiredMsg').fadeIn(); 
+    } else {
+        input.closest('.input-group').find('.requiredMsg').fadeOut();
+    }
+});
+
+$('#lastName').on('blur', function () {
+    var input = $(this);
+    if (input.val() === '') {
+        input.closest('.input-group').find('.requiredMsg').fadeIn();
+    } else {
+        input.closest('.input-group').find('.requiredMsg').fadeOut();
+    }
+});
+
+$('#emailAddress').on('blur', function () {
+    var input = $(this);
+    if (input.val() === '') {
+        input.closest('.input-group').find('.requiredMsg').fadeIn();
+    } else {
+        input.closest('.input-group').find('.requiredMsg').fadeOut();
+    }
+});
+//var msg=input.closest('input-group').find('.requiredMsg');
+//$('#emailAddress').on('blur', function(){
+//var input=$(this);
+//var inputValue=input.val();
+
+//if(inputValue.indexOf('@')===-1|| inputValue.indexOf('.com')===-1){
+//msg.fadeIn();}
+//else {msg.fadeOut();}
